8ab039179113d32aeded5b8b0746eb7cc9a34039
Calebe-Marques <calebe.reboucas@sptech.school>
2025-01-02 22:34:07 -0300

    feat: exercicio das palavras secretas

---
+++ basico/exercicios/for/ex_palavras_secretas.py
@@ -0,0 +1,34 @@
+import os
+
+palavra_secreta = 'Teste'
+palavra_secreta_escondida = ''
+letras_acertadas = ''
+qtd_tentativas = 0
+
+while True:
+
+    letra_escolhida = str(input("Escolha uma letra:\n")).lower()
+
+    if len(letra_escolhida) > 1:
+        print("Escolha uma letra por vez")
+        continue
+
+    if letra_escolhida in palavra_secreta:
+        letras_acertadas += letra_escolhida
+
+
+    palavra_secreta_formada = ''
+    for letra_secreta in palavra_secreta.lower():
+        if letra_secreta in letras_acertadas:
+            palavra_secreta_formada += letra_secreta
+            
+        else:
+            palavra_secreta_formada += '*'
+
+    print(palavra_secreta_formada)
+    qtd_tentativas += 1
+
+    if palavra_secreta.lower() == palavra_secreta_formada:
+        os.system('cls')
+        print(f'Parabéns, você adivinhou a palavra!\nA palavra secreta era: {palavra_secreta}\nTentaivas: {qtd_tentativas}')
+        break
